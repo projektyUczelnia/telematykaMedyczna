375d1c944e41f7ec860eefc12b205889c3969d63
Marta96 <marta.piotrowska9609@gmail.com>
2019-03-07 23:09:55 +0100

    First schemat

--- app/src/main/java/com/hfad/dzienniczekseniora/MainActivity.java
+++ app/src/main/java/com/hfad/dzienniczekseniora/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hfad.dzienniczekseniora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addNote = findViewById(R.id.addNote);
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //przejscie do nowego okna wyboru notatki do dodania do obecnego dnia
+                Intent intentChoiceData = new Intent(MainActivity.this, ChoiceData.class);
+                startActivity(intentChoiceData);
+            }
+        });
+
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                //przejscie do okna wyswietlenia notatek z wybranego dnia
+
+                Intent intentShowData = new Intent(MainActivity.this, ShowData.class);
+                startActivity(intentShowData);
+            }
+        });
     }
 }

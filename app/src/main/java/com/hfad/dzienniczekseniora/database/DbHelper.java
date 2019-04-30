9d28b670409fda4c345f450ac8753a115d6256e7
jas <janekpro996@gmail.com>
2019-04-30 13:08:08 +0200

    database +databaseController

---
+++ app/src/main/java/com/hfad/dzienniczekseniora/database/DbHelper.java
@@ -0,0 +1,72 @@
+package com.hfad.dzienniczekseniora.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.List;
+
+
+public class DbHelper extends SQLiteOpenHelper {
+
+
+    public DbHelper(Context context) {
+        super(context, "dzienniczekSenioraDb", null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTableCommandCerator(EnumTable.TEMPERATURE.returnTableConstValues()));
+        db.execSQL(createTableCommandCerator(EnumTable.GLUCOSE.returnTableConstValues()));
+        db.execSQL(createTableCommandCerator(EnumTable.PRESSURE.returnTableConstValues()));
+        db.execSQL(createTableCommandCerator(EnumTable.WEIGHT.returnTableConstValues()));
+        db.execSQL(createTableCommandCerator(EnumTable.VISIT.returnTableConstValues()));
+        db.execSQL(createTableCommandCerator(EnumTable.OTHER.returnTableConstValues()));
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.TEMPERATURE.returnTableConstValues().get(0) + "'");
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.WEIGHT.returnTableConstValues().get(0) + "'");
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.GLUCOSE.returnTableConstValues().get(0) + "'");
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.PRESSURE.returnTableConstValues().get(0) + "'");
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.OTHER.returnTableConstValues().get(0) + "'");
+        db.execSQL("DROP TABLE IF EXISTS '" + EnumTable.VISIT.returnTableConstValues().get(0) + "'");
+        onCreate(db);
+    }
+
+    private String createTableCommandCerator(List columnsList) {
+        String command = "CREATE TABLE " + columnsList.get(0) + "(ID INTEGER PRIMARY KEY AUTOINCREMENT," + columnsList.get(1) + " TEXT, " + columnsList.get(2) + " TEXT, " + columnsList.get(3) + " TEXT);";
+        return command;
+    }
+
+    public void insert_data(List EnumTableClassList, String date, String time, String value) {
+    }
+
+    public List getTempData(String date) {
+        return null;
+    }
+
+    public List getGlucoseData(String date) {
+        return null;
+    }
+
+    public List getPressureData(String date) {
+        return null;
+    }
+
+    public List getWeightData(String date) {
+        return null;
+    }
+
+    public List getVisitData(String date) {
+        return null;
+    }
+
+    public List getOtherData(String date) {
+        return null;
+    }
+
+
+}
---
+++ app/src/main/java/com/hfad/dzienniczekseniora/database/EnumTable.java
@@ -0,0 +1,79 @@
+package com.hfad.dzienniczekseniora.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public enum EnumTable {
+
+    GLUCOSE() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> glucose = new ArrayList<>();
+            glucose.add("GLUCOSE_TABLE");
+            glucose.add("GLUCOSE_DATE");
+            glucose.add("GLUCOSE_TIME");
+            glucose.add("GLUCOSE_VALUE");
+            return glucose;
+        }
+    },
+    TEMPERATURE() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> temperature = new ArrayList<>();
+            temperature.add("TEMPERATURE_TABLE");
+            temperature.add("TEMPERATURE_DATE");
+            temperature.add("TEMPERATURE_TIME");
+            temperature.add("TEMPERATURE_VALUE");
+            return temperature;
+        }
+    },
+    PRESSURE() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> pressure = new ArrayList<>();
+            pressure.add("PRESSURE_TABLE");
+            pressure.add("GLUCOSE_DATE");
+            pressure.add("PRESSURE_TIME");
+            pressure.add("PRESSURE_VALUE");
+            return pressure;
+        }
+    },
+    VISIT() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> weight = new ArrayList<>();
+            weight.add("WEIGHT_TABLE");
+            weight.add("WEIGHT_DATE");
+            weight.add("WEIGHT_TIME");
+            weight.add("WEIGHT_VALUE");
+            return weight;
+        }
+    },
+    WEIGHT() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> visit = new ArrayList<>();
+            visit.add("VISIT_TABLE");
+            visit.add("VISIT_DATE");
+            visit.add("VISIT_TIME");
+            visit.add("VISIT_VALUE");
+            return visit;
+        }
+    },
+    OTHER() {
+        @Override
+        public List returnTableConstValues() {
+            List<String> others = new ArrayList<>();
+            others.add("OTHER_TABLE");
+            others.add("OTHER_DATE");
+            others.add("OTHER_TIME");
+            others.add("OTHER_VALUE");
+            return others;
+        }
+    };
+
+    public abstract List returnTableConstValues();
+
+
+}
